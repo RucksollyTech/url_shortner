67d65ff99c3ed2be9992f2267b7b4791d6011f39
Rucksolly <rucksolly.tech@gmail.com>
2024-03-02 02:16:24 +0100

    before sleep

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import "@/styles/Navbar.css";
+import "@/styles/Media.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
